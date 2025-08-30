9ad7f55aa22d48e172c4fc8771108a9372eabea9
Itsmevedant145 <cyberboss36@gmail.com>
2025-08-30 23:16:17 +0530

    Fix file import for goalsData

--- frontend/src/Utils/apiPath.js
+++ frontend/src/Utils/apiPath.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:9000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:9000';
+
 
 export const API_Path = {
   AUTH: {
